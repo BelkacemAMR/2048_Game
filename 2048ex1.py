a404671bf50b7e196175427de6ed7bd534c7fc38
Belkacem <abel-and-co@hotmail.com>
2023-01-11 10:25:00 +0100

    Modification mineur

--- 2048ex1.py
+++ 2048ex1.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 
 
-# Exercice 01 :  Initialisation de la grille avec
+# Exercice 01 :  Initialisation \ creation  de la grille de jeu  (4*4):
 
 def init_grid():
     grid = np.zeros((4, 4), dtype=int)  # initilalisation d'ne grille 4*4 par des "0"
