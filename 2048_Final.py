afc4da1a6be10ccfb3c0dd35abd6a514c4014888
Belkacem <abel-and-co@hotmail.com>
2023-01-11 11:25:50 +0100

    Ajout exercice 04 au fichier 2048_Final.py

--- 2048_Final.py
+++ 2048_Final.py
@@ -49,3 +49,16 @@ def rollin_row(row):
                     previous = row[i]
         if previous != None:
                 new_row[j] = previous
+
+# Exercice 4:
+# Script de la fonction rollin qui gére les deplacements sur la grille :
+
+def rollin(grid, direction):
+    if direction == 'l':
+        return np.array([rollin_row(row) for row in grid])
+    elif direction == 'r':
+        return np.array([list(reversed(rollin_row(list(reversed(row))))) for row in grid])
+    elif direction == 'u':
+        return np.transpose(rollin(np.transpose(grid), 'l'))
+    elif direction == 'd':
+        return np.transpose(rollin(np.transpose(grid), 'r'))
