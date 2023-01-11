bc26f0fbcacc9735833e0292d0cf9e67c67313db
Belkacem <abel-and-co@hotmail.com>
2023-01-11 11:22:55 +0100

    Exercice 04 : fonction rollin

--- 2048_EX4.py
+++ 2048_EX4.py
@@ -1,1 +1,12 @@
 # Exercice 4:
+# Script de la fonction rollin qui gére les deplacements sur la grille :
+
+def rollin(grid, direction):
+    if direction == 'l':
+        return np.array([rollin_row(row) for row in grid])
+    elif direction == 'r':
+        return np.array([list(reversed(rollin_row(list(reversed(row))))) for row in grid])
+    elif direction == 'u':
+        return np.transpose(rollin(np.transpose(grid), 'l'))
+    elif direction == 'd':
+        return np.transpose(rollin(np.transpose(grid), 'r'))
