36dccb54b99ed00c7e57e94a73fc2ffa546c7f53
Belkacem <abel-and-co@hotmail.com>
2023-01-11 10:46:41 +0100

    Modification mineure : ajout du "Random"

--- 2048ex02.py
+++ 2048ex02.py
@@ -1,5 +1,7 @@
-# Exercice 02:  Ajout de Nouveaux elements dans la grille.
-
+# Exercice 02:
+# Ajout de Nouveaux elements dans la grille.
+import numpy as np
+import random
 def add_new(grid):
     ajout_valeur = random.choice([2, 2, 2, 2, 4])  # 5 elements 100% 4 elements 80% + 1 element 20%
     position = random.choice(list(
